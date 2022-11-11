780ea6165b41c17a580d048497a1c5af40c0c73e
Will Trussell <wptrussell136@gmail.com>
2022-11-11 15:05:38 -0600

    Skeleton for asyncIO extension

---
+++ ableC-asyncIO/grammars/edu.umn.cs.melt.exts.ableC.asyncIO/Export.sv
@@ -0,0 +1,4 @@
+grammar edu:umn:cs:melt:exts:ableC:asyncIO;
+
+exports edu:umn:cs:melt:exts:ableC:asyncIO:abstractsyntax;
+exports edu:umn:cs:melt:exts:ableC:asyncIO:concretesyntax;
---
+++ ableC-asyncIO/grammars/edu.umn.cs.melt.exts.ableC.asyncIO/concretesyntax/Async.sv
@@ -0,0 +1,3 @@
+grammar edu:umn:cs:melt:exts:ableC:asyncIO:concretesyntax;
+
+terminal Spawn_t 'spawn' lexer classes {Keyword};
